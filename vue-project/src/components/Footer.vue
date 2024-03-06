a1b4471588295733c00dc0cf48238be3fb264bf4
Nikita Nasonov <nm.nasonov@zbrsk.ru>
2024-03-06 14:36:02 +0500

    Вывод меню футера через цикл

--- vue-project/src/components/Footer.vue
+++ vue-project/src/components/Footer.vue
@@ -1,95 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+const topics = ref([
+  {
+    id: 1,
+    title: "Компания",
+    links: ["О нас", "Команда", "Новости", "Блог", "Галерея"]
+  },
+  {
+    id: 2,
+    title: "Яхт-клуб",
+    links: ["Правила", "Стоимость", "Анкета", "FAQ", "Партнеры"]
+  },
+  {
+    id: 3,
+    title: "Услуги",
+    links: ["Аренда яхты", "Свадьба на яхте", "День рождения", "Бизнес-встречи", "Прогулки"]
+  },
+  {
+    id: 4,
+    title: "Регаты",
+    links: ["Корпоративные регаты", "Календарь мероприятий", "Стать участником", "Результаты регат", "Обучение"]
+  }
+])
+</script>
+
 <template>
-    <div class="footer-container">
-        <div class="footer-column">
-            <p class="logo">
-            <img src="../assets/img/logo.svg" width="100" height="106" alt="Логотип яхт клуба">
-            </p>
-            <p class="policy"><a href="#">Политика конфиденциальности</a></p>
-            <p class="copyright">Яхтклуб ©2020 Все права защищены</p>
-        </div>
-        <ul class="footer-navigation-topics">
-            <li class="footer-navigation-topic">
-                <h2>Компания</h2>
-                <ul class="footer-navigation">
-                    <li class="footer-navigation-item">
-                    <a href="#">О нас</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Команда</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Новости</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Блог</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Галерея</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="footer-navigation-topic">
-                <h2>Яхт-клуб</h2>
-                <ul class="footer-navigation">
-                    <li class="footer-navigation-item">
-                    <a href="#">Правила</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Стоимость</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Анкета</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">FAQ</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Партнеры</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="footer-navigation-topic">
-                <h2>Услуги</h2>
-                <ul class="footer-navigation">
-                    <li class="footer-navigation-item">
-                    <a href="#">Аренда яхты</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Свадьба на яхте</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">День рождения</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Бизнес-встречи</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Прогулки</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="footer-navigation-topic">
-                <h2>Регаты</h2>
-                <ul class="footer-navigation">
-                    <li class="footer-navigation-item">
-                    <a href="#">Корпоративные регаты</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Календарь мероприятий</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Стать участником</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Результаты регат</a>
-                    </li>
-                    <li class="footer-navigation-item">
-                    <a href="#">Обучение</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+  <div class="footer-container">
+    <div class="footer-column">
+        <p class="logo">
+        <img src="../assets/img/logo.svg" width="100" height="106" alt="Логотип яхт клуба">
+        </p>
+        <p class="policy"><a href="#">Политика конфиденциальности</a></p>
+        <p class="copyright">Яхтклуб ©2020 Все права защищены</p>
     </div>
+    <ul class="footer-navigation-topics">
+        <li class="footer-navigation-topic" v-for="topic in topics" :key="topic.id">
+          <h2>{{ topic.title }}</h2>
+          <ul class="footer-navigation">
+            <li class="footer-navigation-item" v-for="link in topic.links">
+              <a href="#">{{ link }}</a>
+            </li>
+          </ul>
+        </li>
+    </ul>
+  </div>
 </template>
 
 <style>
